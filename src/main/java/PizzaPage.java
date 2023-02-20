d4b691159f1cc0e6a26ecb85921bf177f1a59cd7
Piotr Ropot <Ropot.Piotr@gmail.com>
2023-02-20 22:50:17 +0300

    refactor pom.xml, PizzaPage, PizzaTest class

--- src/main/java/PizzaPage.java
+++ src/main/java/PizzaPage.java
@@ -3,5 +3,9 @@ public class PizzaPage {
     public static final String BTN_MENU = "/html/body/div[2]/div[1]/div[1]/div[2]/ul/li[1]/a";
     public static final String MENU_PIZZA = "/html/body/div[3]/div[1]/div/div/div/div[3]/div/div[1]/div/div/div[9]/a";
     public static final String BTN_CATALOG_PIZZA = "/html/body/div[3]/div[2]/div/div/div/div/div/ul/li[10]/a";
-    public static final String BTN_ADD_BASKET_MARGARITA = "//*[@id=\"menu-card-364\"]/div[3]/div[3]/div[2]/div[2]/button";
+    public static final String BTN_ADD_BASKET_MARGARITA = "//img[@src='https://cloud.new.terrapizza.by/base_menu/12/img/picca-margarita.jpg']";
+    public static final String ADD_BASKET_MARGARITA = "//*[@id=\"modal-product-content\"]/div/div[2]/div[3]/div[2]/div[2]";
+    public static final String BTN_EXIT="//button[@class='fancybox-button fancybox-close-small']";
+    public static final String CLICK_LABEL_BASKET="//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
+    public static final String CHECK_LABEL_MARGARITA ="(//div[contains(text(),'Пицца Маргарита')])[last()]";
 }
--- src/test/java/PizzaTest.java
+++ src/test/java/PizzaTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.time.Duration;
 
@@ -18,7 +19,7 @@ public class PizzaTest {
 
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1));
         driver.get(PizzaPage.URL);
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1));
+
         WebElement BTN_MENU_ELEMENT = driver.findElement(By.xpath(PizzaPage.BTN_MENU));
         BTN_MENU_ELEMENT.click();
 
@@ -29,9 +30,24 @@ public class PizzaTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1));
         WebElement BTN_ADD_BASKET_MARGARITA_ELEMENT = driver.findElement(By.xpath(PizzaPage.BTN_ADD_BASKET_MARGARITA));
         BTN_ADD_BASKET_MARGARITA_ELEMENT.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1));
+        WebElement ADD_BASKET_MARGARITA_ELEMENT = driver.findElement(By.xpath(PizzaPage.ADD_BASKET_MARGARITA));
+        ADD_BASKET_MARGARITA_ELEMENT.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(1));
+        WebElement BTN_EXIT_ELEMENT = driver.findElement(By.xpath(PizzaPage.BTN_EXIT));
+        BTN_EXIT_ELEMENT.click();
+
+        WebElement CLICK_LABEL_BASKET = driver.findElement(By.xpath(PizzaPage.CLICK_LABEL_BASKET));
+        CLICK_LABEL_BASKET.click();
+
+        WebElement CHECK_LABEL_MARGARITA_ELEMENT = driver.findElement(By.xpath(PizzaPage.CHECK_LABEL_MARGARITA));
+        CHECK_LABEL_MARGARITA_ELEMENT.getText();
+
+        Assert.assertEquals("Пицца Маргарита Классическая 32 см", CHECK_LABEL_MARGARITA_ELEMENT.getText());
         driver.quit();
-        // WebElement BTN_ADD_BASKET_MARGARITA_ELEMENT = driver.findElement(By.xpath(TerrapizzaPage.BTN_ADD_BASKET_MARGARITA));
-        // BTN_ADD_BASKET_MARGARITA_ELEMENT.click();
-        //driver.quit();
+
+
     }
 }
